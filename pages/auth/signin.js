84a27d11c8517bca8a0fd709243549024e4500b5
TJ <web3hrive@protonmail.com>
2022-09-08 23:15:27 -0400

    added user authentication using google cloud, signIn signOut features

---
+++ pages/auth/signin.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Header from '../components/Header';
+import { getProviders, signIn } from 'next-auth/react';
+import Image from 'next/image';
+
+import logo from '../../public/assets/images/boogle-page-logo.png';
+
+export default function SignIn({ providers }) {
+  return (
+    <>
+      <Header />
+      {/* server side rendering function here */}
+      <div className="mt-32">
+        {Object.values(providers).map((providers) => (
+          <div key={providers.name} className="flex flex-col items-center">
+            <Image
+              className="left-2/4"
+              src={logo}
+              layout="fixed"
+              alt="logo"
+              width="272px"
+              height="92px"
+            />
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="rounded-lg bg-blue-500 text-white hover:brightness-105 hover:shadow-md p-3"
+              onClick={() => signIn(providers.id, { callbackUrl: '/' })}
+            >
+              Sign in with {providers.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
--- pages/components/User.js
+++ pages/components/User.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div className="flex items-center justify-center basis-auto flex-grow-0 flex-shrink-1 h-[40px ]">
+        <Image
+          className="rounded-full hover:bg-gray-200 cursor-pointer p-1"
+          layout="intrinsic"
+          width="32px"
+          height="32px"
+          onClick={signOut}
+          src={session.user.image}
+          alt=""
+        />
+      </div>
+    );
+  }
+  return (
+    <>
+      <button
+        className="px-6 py-2 font-medium rounded-md bg-blue-500 text-white hover:brightness-105 hover:shadow-md"
+        onClick={signIn}
+      >
+        Sign In
+      </button>
+    </>
+  );
 }
